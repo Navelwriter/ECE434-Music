07ef52652e6f221cd569b2ef675c6f5e375b4b80
Beagle User <debian@BeagleBone.localdomain>
2024-02-15 21:42:07 +0000

    im better

---
+++ weh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Check if the user is root
+if [ $(id -u) -ne 0 ]; then
+    echo "You must be root to run this script"
+    exit 1
+fi
+
+sleep 10
+export DISPLAY=:0
+cd /home/debian/ECE434-Music
+python3 player.py
